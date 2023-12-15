298e14d24030deb844d1a444dd1093679722a78a
Gursimran <64435674+gursimran1906@users.noreply.github.com>
2023-12-15 18:27:25 +0000

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
 from .models import Category, NewsArticle, UserPreferences, Comment, CustomUser
 
-@admin.register(CustomUser)
+admin.register(CustomUser)
+
+admin.register(Category)
 
 
-@admin.register(Category)
+admin.register(NewsArticle)
 
-
-@admin.register(NewsArticle)
-
-
-@admin.register(UserPreferences)
-
-
-@admin.register(Comment)
-
+admin.register(UserPreferences)
+admin.register(Comment)
